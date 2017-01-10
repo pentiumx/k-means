3fb395d0258582ce810b9a64380c4923dbadbc33
pentiumx <pentiumx@live.jp>
2017-01-10 19:52:38 +0900

    Fix bugs

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+%matplotlib inline
+
 import numpy as np
 import os
 import sys
 import random
 import math
+import matplotlib as plt
 
 radius = 10
-num = 100
-#input_data = [(1, 2), (3, 4), (1, -5), (-2, 3)]
+num = 300
 input_data = np.random.uniform(-radius, radius, (num, 2))
 
 def kmeans(input_data, k):
@@ -15,14 +17,14 @@ def kmeans(input_data, k):
 
     # initialize mean vectors
     prev_means = np.random.uniform(0, 10, (k, 2))
-    cur_means = prev_means
+    cur_means = np.random.uniform(0, 10, (k, 2))
 
     # mapping array showing that which cluster a point belongs to
     clusters = np.random.randint(0, k, input_data.shape[0])
 
     # repeat until there are no changes in mean vectors
     while (prev_means != cur_means).all():
-        prev_means = cur_means
+        prev_means = cur_means.copy()
 
         # Step 1: use the estimated means to classify data samples
         for i, data in enumerate(input_data):
@@ -32,7 +34,7 @@ def kmeans(input_data, k):
             # data => [x, y]
             # check which centroid is the nearest
             for j, mean in enumerate(cur_means):
-                dist = dist(data, mean)
+                dist = distance(data, mean)
                 if dist < min_dist:
                     min_mean = j
                     min_dist = dist
@@ -42,16 +44,21 @@ def kmeans(input_data, k):
 
         # Step 2: replace means with mean of current data samples belonging to cluster i
         for j, mean in enumerate(cur_means):
-            samples = clusters[x == j]
-            cur_means[j] = np.mean(samples)
+            indices = np.where(clusters == j)
+            samples = input_data[indices]
+            if len(samples) != 0:
+                cur_means[j] = np.mean(samples, axis=0)
 
     # return cluster info and means of clusters
     return clusters, cur_means
 
-def dist(p1, p2):
+def distance(p1, p2):
     d1 = p1[0]-p2[0]
     d2 = p1[1]-p2[1]
     return math.sqrt( d1*d1 + d2*d2 )
 
-print (input_data)
-print (kmeans(input_data, 2))
+k = 10
+clusters, means = kmeans(input_data, k)
+print('*'*50)
+print (means)
+print (clusters)
